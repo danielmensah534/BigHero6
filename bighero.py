e34c9e2ca7286172ee45676063d07970ed8f5d1f
danielmensah534 <114818023+danielmensah534@users.noreply.github.com>
2022-10-01 10:15:04 -0400

    Create bighero.py

---
+++ bighero.py
@@ -0,0 +1,6 @@
+helloworld = "Hello!"
+Intro = "I" + " " + "am" + " " + "Baymax,"
+Purpose = "your" + " " + "personal" + " " + "healthcare" + " " + "companion"
+print(helloworld)
+print(Intro)
+print(Purpose)
